7b8af407ad6cb1a7bbe0722f72028399a5f6d277
Daniel Small <piktur.io@gmail.com>
2018-10-04 02:36:39 +1000

    🐛 bug: handle missing relation error, ensure Factory constant set outside loop

--- lib/piktur/spec/factories.rb
+++ lib/piktur/spec/factories.rb
@@ -6,42 +6,67 @@ module Piktur
 
     module Factories
 
-      # @param [ROM::Container] env
-      def self.call(env)
-        return if env.nil?
-        ::Piktur.load_initializer('factories')
-        load!(env)
-      end
+      class << self
 
-      private_class_method def self.load!(env) # rubocop:disable MethodLength
-        return if ::Piktur.env.production?
-
-        ::FactoryBot.definition_file_paths.clear
-
-        ::Piktur.railties.each do |railtie|
-          next if railtie.itself?
-
-          path = railtie.root.join('spec', 'factories')
-          ::FactoryBot.definition_file_paths << path.join('factory_bot')
-
-          if defined?(::ROM::Factory)
-            ::Factory ||= ::ROM::Factory.configure do |config|
-              config.rom = env
-            end.struct_namespace(::Object)
-
-            path = path.join('rom')
-            path.exist? && path.find do |path|
-              next if path.directory?
-              require_relative path
-            end
-          end
+        # @param [ROM::Container] env
+        def call(env)
+          return if env.nil?
+          ::Piktur.load_initializer('factories')
+          load!(env)
         end
 
-        ::FactoryBot.find_definitions unless ::Piktur.rails?
+        private
 
-        true
-      rescue ::ArgumentError, ::LoadError => err
-        ::Piktur.logger.error(err.message)
+          def load!(env)
+            return if ::Piktur.env.production?
+
+            rom(env) if defined?(::ROM::Factory)
+            factory_bot if defined?(::FactoryBot)
+
+            true
+          rescue ::ArgumentError, ::LoadError => err
+            ::Piktur.logger.error(err.message)
+          end
+
+          def rom(env)
+            ::Object.safe_const_reset(
+              :Factory,
+              ::ROM::Factory.configure { |config| config.rom = env }.struct_namespace(::Object)
+            )
+
+            find_paths do |path, railtie|
+              path = path.join('rom')
+
+              path.exist? && path.find do |path|
+                next if path.directory?
+                ::Kernel.load path
+              rescue ::ROM::ElementNotFoundError => err
+                # @todo Likely due to DB.config reset before constants cleared. In this case
+                # the ROM container will not contain the corresponding relation.
+                ::Piktur.logger.error(err.message)
+              ensure
+                next
+              end
+            end
+          end
+
+          def factory_bot
+            ::FactoryBot.definition_file_paths.clear
+
+            find_paths do |path|
+              ::FactoryBot.definition_file_paths << path.join('factory_bot')
+            end
+
+            ::FactoryBot.find_definitions unless ::Piktur.rails?
+          end
+
+          def find_paths
+            ::Piktur.railties.each do |railtie|
+              next if railtie.itself?
+              yield(railtie.root.join('spec', 'factories'), railtie) if block_given?
+            end
+          end
+
       end
 
     end

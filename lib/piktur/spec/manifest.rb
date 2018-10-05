9147df3e47e27745e3d61a829cbc2596ae9296b1
Daniel Small <piktur.io@gmail.com>
2018-10-05 10:54:13 +1000

    💚 test: improve manifest groups definition

--- lib/piktur/spec/manifest.rb
+++ lib/piktur/spec/manifest.rb
@@ -9,41 +9,27 @@ module Piktur::Spec
   #   require 'piktur/spec/manifest'
   #
   #   module Manifest
-  #     include Piktur::Spec::Manifest
-  #
   #     MODELS = ['a_spec.rb', 'b_spec.rb']
   #     FOCUS  = ['component/a', 'feature/z']
+  #     extend Piktur::Spec::Manifest
   #   end
   #
   #   Manifest.models # => ['a_spec.rb', 'b_spec.rb']
   #   Manifest.focus(pattern: '**/application_*') # => ['spec/unit/application_model_spec.rb']
+  #
   module Manifest
 
     # @return [String]
     PATTERN  = 'spec/%s{,/*,/**/*}_spec.rb'
 
+    def self.extended(base)
+      base.constants.each { |group| alias_method group.downcase, :call }
+    end
+
     def call(root: ::Dir.pwd, pattern: nil)
       safe_const_get(__callee__.upcase) ||
         ::Dir[format(PATTERN, (pattern || __callee__)), base: root]
     end
-
-    %i(
-      api
-      assets
-      cache
-      concepts
-      db
-      features
-      focus
-      integration
-      loader
-      models
-      pending
-      policies
-      security
-      support
-      system
-    ).each { |type| alias_method type, :call }
 
   end
 
